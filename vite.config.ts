3d0202f4d01b3fe6f596f835291af878fa19214a
starwhisper-9 <star@sotis.space>
2025-12-10 20:37:31 +0800

    build: 剔除了wasm携带

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "exclude-wasm-plugin",
+      generateBundle(_, bundle) {
+        // 遍历所有即将打包的文件
+        for (const fileName in bundle) {
+          // 如果文件名以 .wasm 结尾（或者你可以匹配特定的 onnxruntime 文件名）
+          if (fileName.endsWith(".wasm")) {
+            console.log(`\n已从构建产物中剔除: ${fileName}`);
+            // 从 bundle 对象中删除，这样 Vite 就不会把它写入 dist 目录了
+            delete bundle[fileName];
+          }
+        }
+      },
+    },
+  ],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
